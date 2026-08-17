b56ddee6049e7fb5f901d56daca0f5e81b7d8236
Siddharth Sharma <ssharma1081@gmail.com>
2026-08-17 18:04:57 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        head = 0
+        tail = len(s) - 1
+        while head <= tail:
+            if not s[head].isalnum():
+                head += 1
+                continue
+            if not s[tail].isalnum():
+                tail -= 1
+                continue
+            if s[head].lower() != s[tail].lower():
+                return False
+            
+            head += 1
+            tail -= 1
+        return True
+
